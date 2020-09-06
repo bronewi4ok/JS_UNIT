d8e6fa931999bd9ef5a1738796b941e14d6b5957
bronewi4ok <bronewi4ok@gmail.com>
2020-09-06 15:55:43 +0300

    Callbacks lesson 19

--- script.js
+++ script.js
@@ -1,74 +1,21 @@
 "use strict";
+// Callback, lesson 19
 
-
-let numberOfFilms;
-
-function start() {
-    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели: ");
-
-    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели: ");
-    }
+function first() {
+    setTimeout(
+        function () {
+            console.log("first");
+        }, 500
+    );
 }
 
-// start();
-
-let personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false,
-};
-
-function rememberMyFilsm() {
-    for (let i = 0; i < 2; i++) {
-        const a = prompt("Один из нескольких просмотренных фильмов: "),
-            b = prompt("На сколько оцените его: ");
-        if (a != null, b != null, a != '', b != '', a.length < 50) {
-            personalMovieDB.movies[a] = b;
-            console.log("siccess");
-        } else {
-            console.log("error");
-            i--;
-        }
-    }
-}
-// rememberMyFilsm();
-
-
-function detectPersonalLevel() {
-    if (personalMovieDB.count < 10) {
-        console.log("Просмотренно довольно мало фильмов");
-    } else if (personalMovieDB.count > 10 || personalMovieDB.count < 50) {
-        console.log("Вы класический зритель");
-    } else if (personalMovieDB.count > 50) {
-        console.log("Вы киноман");
-    } else {
-        console.log("Произошла ошибка");
-    }
+function second() {
+    console.log("second");
 }
 
-// detectPersonalLevel();
-
-
-function showMyDB(hidden) {
-    if (!hidden) {
-        console.log(personalMovieDB);
-    }
-}
-showMyDB(personalMovieDB.privat);
-
-function writeYourGenres(genre) {
-    for (let i = 0; i < 3; i++) {
-        let a = prompt(`Ваш любимый жанр под номером ${i + 1}`);
-        if (a != null && a != "") {
-            genre[i] = a;
-        } else {
-            i--;
-        }
-    }
-    console.log(genre);
+function learnJS(lang, callback) {
+    console.log(`Я учу ${lang}`);
+    callback();
 }
 
-writeYourGenres(personalMovieDB.genres);
+learnJS("JavaScript", second);
